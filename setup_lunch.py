e786d0f47057913f47fd6a7e30409f3e3ce6923b
Tyler Postma <comptvids@gmail.com>
2022-03-31 00:29:25 -0400

    We can now generate MSI files on the Windows side for x64 systems.

--- setup_lunch.py
+++ setup_lunch.py
@@ -5,11 +5,66 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
+directory_table = [
+    ("ProgramMenuFolder", "TARGETDIR", "."),
+    ("MyProgramMenu", "ProgramMenuFolder", "MYPROG~1|Miijin Lunch"),
+]
+
+shortcut_table = [
+    ("DesktopShortcut",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "Miijin Lunch",           # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]MiijinReport.exe",  # Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     ),
+    ("StartMenuShortcut",        # Shortcut
+     "ProgramMenuFolder",          # Directory_
+     "Miijin Lunch",           # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]MiijinLunch.exe",  # Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     )
+    ]
+
+msi_data = {
+    "Directory": directory_table,
+    "ProgId": [
+        ("Prog.Id", None, None, "Miijin Lunch Program", "IconId", None),
+    ],
+    "Icon": [
+        ("IconId", "icons/MiijinLunchWindows.ico"),
+    ],
+    "Shortcut": shortcut_table
+}
+
+bdist_msi_options = {
+    "add_to_path": True,
+    "data": msi_data
+}
+
 setup(
         name="Miijin Lunch",
         version="0.1",
+        author='Tyler Postma',
+        author_email='tpostma@bmcc.edu',
+        url='https://github.com/BayMillsCC/Miijin',
         description="Scans IDs into SQL database",
-        icon='icons/MiijinLunchWindows.ico',
-        executables=[Executable("src/Miijin/MiijinLunch/MiijinLunch.py", base=base)],
-        options={'bdist_mac': {'iconfile': 'icons/MiijinLunch.icns'}}
+        executables=[Executable("src/Miijin/MiijinLunch/MiijinLunch.py", base=base,
+                                icon="icons/MiijinLunchWindows.ico"
+                                )],
+        options={'bdist_mac': {'iconfile': 'icons/MiijinLunch.icns'},
+                 'bdist_msi': bdist_msi_options}
 )
--- setup_report.py
+++ setup_report.py
@@ -5,11 +5,67 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
+directory_table = [
+    ("ProgramMenuFolder", "TARGETDIR", "."),
+    ("MyProgramMenu", "ProgramMenuFolder", "MYPROG~1|Miijin Report"),
+]
+
+shortcut_table = [
+    ("DesktopShortcut",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "Miijin Report",           # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]MiijinReport.exe",  # Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     ),
+    ("StartMenuShortcut",        # Shortcut
+     "ProgramMenuFolder",          # Directory_
+     "Miijin Report",           # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]MiijinReport.exe",  # Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     )
+    ]
+
+msi_data = {
+    "Directory": directory_table,
+    "ProgId": [
+        ("Prog.Id", None, None, "Miijin Report Program", "IconId", None),
+    ],
+    "Icon": [
+        ("IconId", "icons/MiijinReportWindows.ico"),
+    ],
+    "Shortcut": shortcut_table
+}
+
+bdist_msi_options = {
+    "add_to_path": True,
+    "data": msi_data
+}
+
+
 setup(
         name="Miijin Report",
         version="0.1",
+        author='Tyler Postma',
+        author_email='tpostma@bmcc.edu',
+        url='https://github.com/BayMillsCC/Miijin',
         description="Pulls Data from SQL into Excel File",
-        icon='icons/MiijinReportWindows.ico',
-        executables=[Executable("src/Miijin/MiijinReport/MiijinReport.py", base=base)],
-        options={'bdist_mac': {'iconfile': 'icons/MiijinReport.icns'}}
+        executables=[Executable("src/Miijin/MiijinReport/MiijinReport.py", base=base,
+                                icon="icons/MiijinReportWindows.ico"
+                                )],
+        options={'bdist_mac': {'iconfile': 'icons/MiijinReport.icns'},
+                 'bdist_msi': bdist_msi_options}
 )
