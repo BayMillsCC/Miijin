3efcce40d5db62fe31d78706240733794f9028eb
Tyler Postma <tpostma@bmcc.edu>
2025-12-29 23:49:39 -0500

    Additional SQL Creation

--- src/Miijin/MiijinDatabase/MiijinDBInitialization.sql
+++ src/Miijin/MiijinDatabase/MiijinDBInitialization.sql
@@ -1,3 +1,9 @@
+
+--CREATE DATABASE miijinprod;
+
+
+\c miijinprod
+
 CREATE SCHEMA IF NOT EXISTS miijinprod;
 
 CREATE TABLE IF NOT EXISTS miijinprod.studentlunchrecords
@@ -15,13 +21,45 @@ CREATE TABLE IF NOT EXISTS miijinprod.employeelunchrecords
 );
 
 CREATE INDEX IF NOT EXISTS idx_studentlunch_time
-ON miijinprod.studentlunchrecords (timeidscanned);
+    ON miijinprod.studentlunchrecords (timeidscanned);
 
 CREATE INDEX IF NOT EXISTS idx_studentlunch_student
-ON miijinprod.studentlunchrecords (studentid);
+    ON miijinprod.studentlunchrecords (studentid);
 
 CREATE INDEX IF NOT EXISTS idx_employeelunch_time
-ON miijinprod.employeelunchrecords (timeidscanned);
+    ON miijinprod.employeelunchrecords (timeidscanned);
 
 CREATE INDEX IF NOT EXISTS idx_employeelunch_employee
-ON miijinprod.employeelunchrecords (employeeid);
+    ON miijinprod.employeelunchrecords (employeeid);
+
+CREATE ROLE miijin_app
+WITH
+    LOGIN
+    PASSWORD 'SUPERSECRETPASSWORD'
+    NOSUPERUSER
+    NOCREATEDB
+    NOCREATEROLE
+    NOINHERIT;
+
+GRANT CONNECT ON DATABASE miijinprod TO miijin_app;
+
+GRANT USAGE ON SCHEMA miijinprod TO miijin_app;
+
+GRANT
+    SELECT,
+    INSERT
+ON TABLE
+    miijinprod.studentlunchrecords,
+    miijinprod.employeelunchrecords
+TO miijin_app;
+
+GRANT USAGE, SELECT
+ON ALL SEQUENCES IN SCHEMA miijinprod
+TO miijin_app;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA miijinprod
+GRANT SELECT, INSERT ON TABLES TO miijin_app;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA miijinprod
+GRANT USAGE, SELECT ON SEQUENCES TO miijin_app;
+
